9bead9d9cad1e2539edc8eb151b8d962bd547c43
THVstars <kcarolinasanchez@gmail.com>
2022-01-22 01:28:50 -0500

    Updated the Main class.

--- src/com/carolinasanchez/Main.java
+++ src/com/carolinasanchez/Main.java
@@ -19,20 +19,20 @@ public class Main {
         dealership.displayInventory();
 
         System.out.println(" ");
-        System.out.println("Would you like to: \n1. Book a car \n2. Return a car \n3. Exit the dealership? Please enter a number.");
+        System.out.println("Would you like to: \n1. Book a car \n2. Return a car \n3. Exit the dealership? \nPlease enter a number.");
         System.out.println(" ");
         int response = scanner.nextInt();
 
         while (response == 1) {
             dealership.bookACar();
             System.out.println(" ");
-            System.out.println("Would you like to: \n1. Book a car \n2. Return a car \n3. Exit the dealership? Please enter a number.");
+            System.out.println("Would you like to: \n1. Book a car \n2. Return a car \n3. Exit the dealership? \nPlease enter a number.");
             System.out.println(" ");
             response = scanner.nextInt();
         } while (response == 2) {
             dealership.returnACar();
             System.out.println(" ");
-            System.out.println("Would you like to: \n1. Book a car \n2. Return a car \n3. Exit the dealership? Please enter a number.");
+            System.out.println("Would you like to: \n1. Book a car \n2. Return a car \n3. Exit the dealership? \nPlease enter a number.");
             System.out.println(" ");
             response = scanner.nextInt();
         }
